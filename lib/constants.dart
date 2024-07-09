9a46e379b7b757bb80f18d6bf6192a89bd87edcb
thinHtetSan22 <120102108+thinHtetSan22@users.noreply.github.com>
2024-07-09 07:28:51 +0630

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+class Constants{
+  static const apiKey = 'a0116c1b032a96bbf719502d6cfdf68b';
+
+  static const imagePath = 'https://image.tmdb.org/t/p/w500';
+}
---
+++ lib/widgets/movies_slider.dart
@@ -0,0 +1,57 @@
+import 'package:flutflix/constants.dart';
+import 'package:flutflix/screens/details_screen.dart';
+import 'package:flutter/material.dart';
+
+class MoviesSlider extends StatelessWidget {
+  const MoviesSlider({
+    super.key, required this.snapshot,
+  });
+
+  final AsyncSnapshot snapshot;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      width: double.infinity,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        physics: const BouncingScrollPhysics(),
+        itemCount: snapshot.data!.length,
+        
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap:(){
+                Navigator.push(context, 
+                MaterialPageRoute(
+                  builder:(context) => DetailsScreen(
+                    movie: snapshot.data!
+                    [index],
+                  ),
+                ),
+              );
+              },
+
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox(
+                  
+                  height: 200,
+                  width: 200,
+                  child: Image.network(
+                    filterQuality: FilterQuality.high,
+                    fit: BoxFit.cover,
+                    '${Constants.imagePath}${snapshot.data![index].posterPath}'
+                  )
+                ),
+              ),
+            ),
+            
+          );
+        }
+      ),
+    );
+  }
+}
